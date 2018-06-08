09acbdba1c67b92f9ddd456237f53d814dd90e8b
Khetam-He <toty.f.h@hotmail.com>
2018-06-08 07:27:26 +0300

    add gulp file

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
+gulp.task('connect', function(){
+  connect.server({
+  port: 9997
+});
+});
+gulp.task('copy', function () {
+  return mergeStream(
+       gulp.src('data/*.json').pipe(gulp.dest('data/'))
+  );
+});
+
+gulp.task('watch', function () {
+   gulp.watch([ 'data/*.json'], ['copy']);
+
+  Object.keys(jsBundles).forEach(function(key) {
+    var b = jsBundles[key];
+    b.on('update', function() {
+      return bundle(b, key);
+    });
+  });
+});
+
+gulp.task('default',['connect']);
+  
