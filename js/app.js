320bbffabce8bbd31dceedec6a68df249bf14696
Khetam-He <toty.f.h@hotmail.com>
2018-06-08 08:28:29 +0300

    formatting the code

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-if('serviceWorker'in navigator){
+if ('serviceWorker' in navigator) {
 
-navigator.serviceWorker
-.register('./service-Worker.js',{scope:'./'})
-.then(function (registration) {
-console.log("service Worker Registered",registration);
-})
-.catch(function (err) {
-console.log("service Worker failed to Register",err);
-})
+    navigator.serviceWorker
+        .register('./service-Worker.js', {
+            scope: './'
+        })
+        .then(function(registration) {
+            console.log("service Worker Registered", registration);
+        })
+        .catch(function(err) {
+            console.log("service Worker failed to Register", err);
+        })
 
 }
--- service-Worker.js
+++ service-Worker.js
@@ -1,84 +1,78 @@
 var staticCacheName = 'restauravt-static-v1';
 
 var cacheFiles = [
-'./',
- './restaurant.html',
- 'js/main.js',
- 'js/dbhelper.js',
- 'js/app.js',
- 'js/restaurant_info.js',
- 'css/styles.css',
- 'css/responsive.css',
- 'css/restaurant-responsive.css',
- 'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
- 'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+    './',
+    './restaurant.html',
+    'data/restaurants.json',
+    'js/main.js',
+    'js/dbhelper.js',
+    'js/app.js',
+    'js/restaurant_info.js',
+    'css/styles.css',
+    'css/responsive.css',
+    'css/restaurant-responsive.css',
+    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+    'https://maps.googleapis.com/maps/api/js?key=AIzaSyDfQPpFKfgkuZ9ZDYmNy165S4IAZauv4K4&libraries=places&callback=initMap',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
 ];
 
 self.addEventListener('install', function(event) {
-  // TODO: cache /skeleton rather than the root page
-  console.log('service worker installed');
+    // TODO: cache /skeleton rather than the root page
+    console.log('service worker installed');
 
-  event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-        console.log('[service worker] Caching  cacheFiles');
+    event.waitUntil(
+        caches.open(staticCacheName).then(function(cache) {
+            console.log('[service worker] Caching  cacheFiles');
 
-      return cache.addAll(cacheFiles);
-    })
-  );
+            return cache.addAll(cacheFiles);
+        })
+    );
 });
 
 self.addEventListener('activate', function(event) {
-   console.log('service worker activated');
+    console.log('service worker activated');
 
 
-event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('restauravt-') &&
-                 cacheName != staticCacheName;
-                 console.log('[service worker] Removing cache files from',cacheName);
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restauravt-') &&
+                        cacheName != staticCacheName;
+                    console.log('[service worker] Removing cache files from', cacheName);
 
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
 
-/*
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-          return Promise.all(cacheNames.map(function(thisCacheName){
-
-            if(thisCacheName !== cacheName){
-                 console.log('[service worker] Removing cache files from',thisCacheName);
-                 return caches.delete(thisCacheName);
-
-            }
-
-          })) 
-        })
-      );
-    })*/
-
-
 self.addEventListener('fetch', function(event) {
-  console.log('service worker fetched');
-/*
-  var requestUrl = new URL(event.request.url);
-  if(requestUrl.origin === location.origin){
-    if(requestUrl.pathname === '/'){
-      event.respondWith(caches.match('/skeleton'));
-      return;
-    }
+    console.log('service worker fetched');
 
-  }
+    event.respondWith(
 
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );*/
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                console.log('[service worker] found in cache', event.request.url);
+                return response;
+            }
+            var requestClone = event.request.clone();
+            fetch(requestClone)
+                .then(function(response) {
+                    if (!response) {
+                        console.log('[service worker] no response from fetch');
+                        return response;
+                    }
+                    var responseClone = response.clone();
+                    caches.open(staticCacheName).then(function(cache) {
+                        cache.put(event.request, responseClone);
+                        return response;
+                    });
+                })
+        })
+    )
+
 });
